9cf90a687b1d55b450de587d97e609173e75dbaf
KujawskiCezary <kujawskicezary@wp.pl>
2017-10-10 20:54:26 +0200

    Init

---
+++ constants.py
@@ -0,0 +1,19 @@
+#                               __                    __             
+#                              /\ \__                /\ \__          
+#   ___    ___     ___     ____\ \ ,_\    __      ___\ \ ,_\   ____  
+#  /'___\ / __`\ /' _ `\  /',__\\ \ \/  /'__`\  /' _ `\ \ \/  /',__\ 
+# /\ \__//\ \L\ \/\ \/\ \/\__, `\\ \ \_/\ \L\.\_/\ \/\ \ \ \_/\__, `\
+# \ \____\ \____/\ \_\ \_\/\____/ \ \__\ \__/.\_\ \_\ \_\ \__\/\____/
+#  \/____/\/___/  \/_/\/_/\/___/   \/__/\/__/\/_/\/_/\/_/\/__/\/___/  .txt
+#
+#
+
+CASC_PATH = './haarcascade_files/haarcascade_frontalface_default.xml'
+SIZE_FACE = 48
+EMOTIONS = ['angry', 'disgusted', 'fearful', 'happy', 'sad', 'surprised', 'neutral']
+SAVE_DIRECTORY = './data/'
+SAVE_MODEL_FILENAME = 'Gudi_model_100_epochs_20000_faces.data-00000-of-00001'
+SAVE_DATASET_IMAGES_FILENAME = 'data_kike.npy'
+SAVE_DATASET_LABELS_FILENAME = 'labels_kike.npy'
+SAVE_DATASET_IMAGES_TEST_FILENAME = 'test_data_kike.npy'
+SAVE_DATASET_LABELS_TEST_FILENAME = 'test_labels_kike.npy'

b8669a89bb01f8834f421a53af112466a7b7b73d
Damian W <winczewski.d@gmail.com>
2017-12-20 11:31:36 +0100

    refactor ui

--- ui2.py
+++ ui2.py
@@ -11,7 +11,7 @@ from enum import Enum
 from sklearn.externals import joblib
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 
-from PyQt5 import QtCore, QtGui, QtWidgets, QtWebKitWidgets, QtMultimedia
+from PyQt5 import QtCore, QtGui, QtWidgets, QtWebEngine, QtMultimedia
 
 import cv2
 import numpy as np
@@ -185,6 +185,7 @@ class Ui_Dialog():
 
     def startCaptureCamera(self):
         self.url = 0
+
         try:
             self.startCapture()
         except Exception as err:
@@ -237,21 +238,21 @@ class Ui_Dialog():
     def setupUi(self, Dialog):
             Dialog.setObjectName("Dialog")
             # rozmiar okna
-            Dialog.resize(954, 601)
+            Dialog.resize(240, 480)
 
             # tworzenie ramek na przyciski
             self.frame = self.setUpFrame(Dialog, "camera", (10, 10, 241, 461))
-            self.frame_2 = self.setUpFrame(Dialog, "movie", (270, 10, 261, 271))
+            self.frame_2 = self.setUpFrame(Dialog, "movie", (10, 10, 261, 271))
             self.frame_3 = self.setUpFrame(Dialog, "photo", (270, 290, 261, 181))
 
             # camera buttons
             self.start_button = QtWidgets.QPushButton(self.frame)
-            self.start_button.setGeometry(QtCore.QRect(0, 0, 181, 61))
+            self.start_button.setGeometry(QtCore.QRect(10, 10, 100, 50))
             self.start_button.setObjectName("start")
             self.start_button.clicked.connect(self.startCaptureCamera)
 
             self.stop = QtWidgets.QPushButton(self.frame)
-            self.stop.setGeometry(QtCore.QRect(0, 70, 181, 61))
+            self.stop.setGeometry(QtCore.QRect(130, 10, 100, 50))
             self.stop.setObjectName("stop")
             self.start_button.clicked.connect(self.endCapture)
 
@@ -259,30 +260,30 @@ class Ui_Dialog():
             self.stop.clicked.connect(self.endCapture)
 
             # Photo
-            self.photoImage = QtWidgets.QLabel(self.frame_3)
-            self.photoImage.setGeometry(QtCore.QRect(0, 60, 700, 500))
+            self.photoImage = QtWidgets.QLabel()
+            self.photoImage.setGeometry(QtCore.QRect(10, 10, 220, 50))
 
             self.get_photo_button = QtWidgets.QPushButton(self.frame_3)
-            self.get_photo_button.setGeometry(QtCore.QRect(120, 0, 60, 61))
+            self.get_photo_button.setGeometry(QtCore.QRect(10, 10, 220, 50))
             self.get_photo_button.setObjectName("start")
             self.get_photo_button.clicked.connect(self.openFileNameDialogPhoto)
 
             #video
             # movies buttons
             self.start_movie_button = QtWidgets.QPushButton(self.frame_2)
-            self.start_movie_button.setGeometry(QtCore.QRect(500, 0, 181, 61))
+            self.start_movie_button.setGeometry(QtCore.QRect(10, 10, 100, 50))
             self.start_movie_button.setObjectName("start")
             self.start_movie_button.clicked.connect(self.openFileNameDialog)
 
             self.stop_movie_button = QtWidgets.QPushButton(self.frame_2)
-            self.stop_movie_button.setGeometry(QtCore.QRect(500, 70, 181, 61))
+            self.stop_movie_button.setGeometry(QtCore.QRect(130, 10, 100, 50))
             self.stop_movie_button.setObjectName("stop")
             # self.stop_movie_button.clicked.connect(self.stopPlaying)
             self.stop_movie_button.clicked.connect(self.endCapture)
 
             self.tabWidget = QtWidgets.QTabWidget(Dialog)
             self.tabWidget.setEnabled(True)
-            self.tabWidget.setGeometry(QtCore.QRect(0, 0, 701, 561))
+            self.tabWidget.setGeometry(QtCore.QRect(0, 0, 250, 90))
             self.tabWidget.setObjectName("tabWidget")
 
             self.tabWidget.addTab(self.frame, "")
@@ -290,90 +291,90 @@ class Ui_Dialog():
             self.tabWidget.addTab(self.frame_3, "")
 
             self.smutek_progress = QtWidgets.QProgressBar(Dialog)
-            self.smutek_progress.setGeometry(QtCore.QRect(820, 50, 118, 23))
+            self.smutek_progress.setGeometry(QtCore.QRect(120, 100, 118, 23))
             self.smutek_progress.setProperty("value", 24)
             self.smutek_progress.setObjectName("smutek_progress")
             self.smutek_label = QtWidgets.QLabel(Dialog)
-            self.smutek_label.setGeometry(QtCore.QRect(730, 60, 47, 13))
+            self.smutek_label.setGeometry(QtCore.QRect(30, 100, 47, 13))
             self.smutek_label.setObjectName("smutek_label")
             self.zaskoczenie_label = QtWidgets.QLabel(Dialog)
-            self.zaskoczenie_label.setGeometry(QtCore.QRect(730, 100, 71, 16))
+            self.zaskoczenie_label.setGeometry(QtCore.QRect(30, 130, 71, 13))
             self.zaskoczenie_label.setObjectName("zaskoczenie_label")
             self.zaskoczenie_progress = QtWidgets.QProgressBar(Dialog)
-            self.zaskoczenie_progress.setGeometry(QtCore.QRect(820, 90, 118, 23))
+            self.zaskoczenie_progress.setGeometry(QtCore.QRect(120, 130, 118, 23))
             self.zaskoczenie_progress.setProperty("value", 24)
             self.zaskoczenie_progress.setObjectName("zaskoczenie_progress")
             self.strach_label = QtWidgets.QLabel(Dialog)
-            self.strach_label.setGeometry(QtCore.QRect(730, 140, 47, 13))
+            self.strach_label.setGeometry(QtCore.QRect(30, 160, 47, 13))
             self.strach_label.setObjectName("strach_label")
             self.strach_progress = QtWidgets.QProgressBar(Dialog)
-            self.strach_progress.setGeometry(QtCore.QRect(820, 130, 118, 23))
+            self.strach_progress.setGeometry(QtCore.QRect(120, 160, 118, 23))
             self.strach_progress.setProperty("value", 24)
             self.strach_progress.setObjectName("strach_progress")
             self.podziw_label = QtWidgets.QLabel(Dialog)
-            self.podziw_label.setGeometry(QtCore.QRect(730, 180, 47, 13))
+            self.podziw_label.setGeometry(QtCore.QRect(30, 190, 47, 13))
             self.podziw_label.setObjectName("podziw_label")
             self.podziw_progress = QtWidgets.QProgressBar(Dialog)
-            self.podziw_progress.setGeometry(QtCore.QRect(820, 170, 118, 23))
+            self.podziw_progress.setGeometry(QtCore.QRect(120, 190, 118, 23))
             self.podziw_progress.setProperty("value", 24)
             self.podziw_progress.setObjectName("podziw_progress")
             self.radosc_label = QtWidgets.QLabel(Dialog)
-            self.radosc_label.setGeometry(QtCore.QRect(730, 220, 47, 13))
+            self.radosc_label.setGeometry(QtCore.QRect(30, 220, 47, 13))
             self.radosc_label.setObjectName("radosc_label")
             self.radosc_progress = QtWidgets.QProgressBar(Dialog)
-            self.radosc_progress.setGeometry(QtCore.QRect(820, 210, 118, 23))
+            self.radosc_progress.setGeometry(QtCore.QRect(120, 220, 118, 23))
             self.radosc_progress.setProperty("value", 24)
             self.radosc_progress.setObjectName("radosc_progress")
             self.anger_label = QtWidgets.QLabel(Dialog)
-            self.anger_label.setGeometry(QtCore.QRect(730, 251, 91, 20))
+            self.anger_label.setGeometry(QtCore.QRect(30, 250, 91, 20))
             self.anger_label.setObjectName("anger_label")
             self.anger_progress = QtWidgets.QProgressBar(Dialog)
-            self.anger_progress.setGeometry(QtCore.QRect(820, 250, 118, 23))
+            self.anger_progress.setGeometry(QtCore.QRect(120, 250, 118, 23))
             self.anger_progress.setProperty("value", 24)
             self.anger_progress.setObjectName("anger_progress")
             self.spokoj_progress = QtWidgets.QProgressBar(Dialog)
-            self.spokoj_progress.setGeometry(QtCore.QRect(820, 289, 118, 23))
+            self.spokoj_progress.setGeometry(QtCore.QRect(120, 280, 118, 23))
             self.spokoj_progress.setProperty("value", 24)
             self.spokoj_progress.setObjectName("spokoj_progress")
             self.spokoj_label = QtWidgets.QLabel(Dialog)
-            self.spokoj_label.setGeometry(QtCore.QRect(730, 290, 91, 20))
+            self.spokoj_label.setGeometry(QtCore.QRect(30, 280, 91, 20))
             self.spokoj_label.setObjectName("spokoj_label")
             self.obrzydzenie_progress = QtWidgets.QProgressBar(Dialog)
-            self.obrzydzenie_progress.setGeometry(QtCore.QRect(820, 329, 118, 23))
+            self.obrzydzenie_progress.setGeometry(QtCore.QRect(120, 310, 118, 23))
             self.obrzydzenie_progress.setProperty("value", 24)
             self.obrzydzenie_progress.setObjectName("obrzydzenie_progress")
             self.obrzydzenie_label = QtWidgets.QLabel(Dialog)
-            self.obrzydzenie_label.setGeometry(QtCore.QRect(730, 330, 91, 20))
+            self.obrzydzenie_label.setGeometry(QtCore.QRect(30, 310, 91, 20))
             self.obrzydzenie_label.setObjectName("obrzydzenie_label")
 
-            self.radioCamera = QtWidgets.QRadioButton(Dialog)
-            self.radioCamera.setGeometry(QtCore.QRect(700, 520, 41, 17))
-            self.radioCamera.setObjectName("radioCamera")
-            self.radioCameraLabel = QtWidgets.QLabel(Dialog)
-            self.radioCameraLabel.setGeometry(QtCore.QRect(720, 510, 101, 31))
-            self.radioCameraLabel.setObjectName("radioCameraLabel")
-            self.radioCamera.setChecked(True)
-
-
-            self.radioMovie = QtWidgets.QRadioButton(Dialog)
-            self.radioMovie.setGeometry(QtCore.QRect(700, 540, 41, 17))
-            self.radioMovie.setObjectName("radioMovie")
-            self.radioMovieLabel = QtWidgets.QLabel(Dialog)
-            self.radioMovieLabel.setGeometry(QtCore.QRect(720, 530, 101, 31))
-            self.radioMovieLabel.setObjectName("radioCameraLabel")
-
-            self.radioPhoto = QtWidgets.QRadioButton(Dialog)
-            self.radioPhoto.setGeometry(QtCore.QRect(700, 560, 41, 17))
-            self.radioPhoto.setObjectName("radioPhoto")
-            self.radioPhotoLabel = QtWidgets.QLabel(Dialog)
-            self.radioPhotoLabel.setGeometry(QtCore.QRect(720, 550, 101, 31))
-            self.radioPhotoLabel.setObjectName("radioCameraLabel")
+            # self.radioCamera = QtWidgets.QRadioButton(Dialog)
+            # self.radioCamera.setGeometry(QtCore.QRect(0, 520, 41, 17))
+            # self.radioCamera.setObjectName("radioCamera")
+            # self.radioCameraLabel = QtWidgets.QLabel(Dialog)
+            # self.radioCameraLabel.setGeometry(QtCore.QRect(20, 510, 101, 31))
+            # self.radioCameraLabel.setObjectName("radioCameraLabel")
+            # self.radioCamera.setChecked(True)
+            #
+            #
+            # self.radioMovie = QtWidgets.QRadioButton(Dialog)
+            # self.radioMovie.setGeometry(QtCore.QRect(0, 540, 41, 17))
+            # self.radioMovie.setObjectName("radioMovie")
+            # self.radioMovieLabel = QtWidgets.QLabel(Dialog)
+            # self.radioMovieLabel.setGeometry(QtCore.QRect(20, 530, 101, 31))
+            # self.radioMovieLabel.setObjectName("radioCameraLabel")
+            #
+            # self.radioPhoto = QtWidgets.QRadioButton(Dialog)
+            # self.radioPhoto.setGeometry(QtCore.QRect(0, 560, 41, 17))
+            # self.radioPhoto.setObjectName("radioPhoto")
+            # self.radioPhotoLabel = QtWidgets.QLabel(Dialog)
+            # self.radioPhotoLabel.setGeometry(QtCore.QRect(20, 550, 101, 31))
+            # self.radioPhotoLabel.setObjectName("radioCameraLabel")
 
 
             self.emotionIcon = QtWidgets.QLabel(Dialog)
             pixmap = QPixmap('emojis/spokoj.png')
             self.emotionIcon.setPixmap(pixmap)
-            self.emotionIcon.setGeometry(QtCore.QRect(820, 390, 120, 120))
+            self.emotionIcon.setGeometry(QtCore.QRect(70, 340, 120, 120))
 
 
             self.retranslateUi(Dialog)
@@ -383,28 +384,28 @@ class Ui_Dialog():
             _translate = QtCore.QCoreApplication.translate
             # nadanie przyciskom tekst
             Dialog.setWindowTitle(_translate("Dialog", "StreetView"))
-            self.tabWidget.setTabText(self.tabWidget.indexOf(self.frame), _translate("Dialog", "Camera"))
-            self.tabWidget.setTabText(self.tabWidget.indexOf(self.frame_2), _translate("Dialog", "Movie"))
-            self.tabWidget.setTabText(self.tabWidget.indexOf(self.frame_3), _translate("Dialog", "Photo"))
+            self.tabWidget.setTabText(self.tabWidget.indexOf(self.frame), _translate("Dialog", "Kamera"))
+            self.tabWidget.setTabText(self.tabWidget.indexOf(self.frame_2), _translate("Dialog", "Film"))
+            self.tabWidget.setTabText(self.tabWidget.indexOf(self.frame_3), _translate("Dialog", "Zdjecie"))
             self.smutek_label.setText(_translate("Dialog", "smutek"))
             self.zaskoczenie_label.setText(_translate("Dialog", "zaskoczenie"))
             self.strach_label.setText(_translate("Dialog", "strach"))
             self.podziw_label.setText(_translate("Dialog", "podziw"))
             self.radosc_label.setText(_translate("Dialog", "radosc"))
-            self.anger_label.setText(_translate("Dialog", "anger"))
+            self.anger_label.setText(_translate("Dialog", "zlosc"))
             self.spokoj_label.setText(_translate("Dialog", "spokoj"))
             self.obrzydzenie_label.setText(_translate("Dialog", "obrzydzenie"))
-            self.radioCameraLabel.setText(_translate("Dialog", "camera"))
-            self.radioMovieLabel.setText(_translate("Dialog", "movie"))
-            self.radioPhotoLabel.setText(_translate("Dialog", "photo"))
-            self.start_button.setText(_translate("Dialog", "Start camera"))
-            self.get_photo_button.setText(_translate("Dialog", "get photo"))
+            # self.radioCameraLabel.setText(_translate("Dialog", "camera"))
+            # self.radioMovieLabel.setText(_translate("Dialog", "movie"))
+            # self.radioPhotoLabel.setText(_translate("Dialog", "photo"))
+            self.start_button.setText(_translate("Dialog", "Kamera"))
+            self.get_photo_button.setText(_translate("Dialog", "Pobierz zdjecie"))
             # self.play_movie_button.setText(_translate("Dialog", "play"))
-            self.stop.setText(_translate("Dialog", "Stop camera"))
-            self.start_movie_button.setText(_translate("Dialog", "Get movie"))
+            self.stop.setText(_translate("Dialog", "Stop"))
+            self.start_movie_button.setText(_translate("Dialog", "Pobierz film"))
             # self.readEmotions.setText(_translate("Dialog", "Start read emotions"))
             # self.stopReadEmotions.setText(_translate("Dialog", "Stop read emotions"))
-            self.stop_movie_button.setText(_translate("Dialog", "Stop movie"))
+            self.stop_movie_button.setText(_translate("Dialog", "Stop"))
 
 
     def setUpFrame(self, Dialog, name, geometry):
